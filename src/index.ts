1feca33221c10ab8a058b119b9bd833dd3291dd3
myyrakle <sssang97@naver.com>
2021-04-15 11:06:14 +0900

    0.4.0 타입 문제 수정

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,5 @@
 import { codegen } from '@myyrakle/swagger-axios-codegen/dist'
-import { writeFile, readFile } from 'fs'
-import { promisify } from 'util'
 import { cloneDeep } from 'lodash'
-
-const writeFileAsync = promisify(writeFile)
-const readFileAsync = promisify(readFile)
-
-export interface generateParemeter {}
 
 export async function generateAxios(document: any, basePath: string) {
   const documentObject: any = cloneDeep(document)
